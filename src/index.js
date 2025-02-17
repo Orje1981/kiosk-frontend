151e7f610b9becdb9c2da4afba8b04af020daf1b
Orje1981 <orlando.froehlich@gmail.com>
2025-02-18 00:45:17 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-
 import React from "react";
-import ReactDOM from "react-dom";
-import KioskControlDashboard from "./KioskControlDashboard";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.render(<KioskControlDashboard />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
