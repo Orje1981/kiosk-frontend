cc54f673c7875b249a618dcd6877b8400a1c4ea4
Orje1981 <orlando.froehlich@gmail.com>
2025-02-18 00:03:31 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import KioskControlDashboard from "./KioskControlDashboard";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <KioskControlDashboard />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
