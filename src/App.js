33c71288541441740aec9bea3a8fe51ff4a56acc
Orje1981 <orlando.froehlich@gmail.com>
2025-02-18 00:37:33 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Willkommen im Kiosk-Frontend</h1>
+      <p>Die Verbindung zum Backend läuft!</p>
+    </div>
+  );
+}
+
+export default App;
